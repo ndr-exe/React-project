b29672c13e7e72850d051e6e090bdeac05569964
ndr-exe <miminodar@Nodars-MacBook-Pro.local>
2024-03-10 23:42:10 +0400

    added base Content & structure & styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,388 @@
-import logo from './logo.svg';
-import './App.css';
+import {Bars3Icon,XMarkIcon,CheckIcon} from "@heroicons/react/24/outline"
+import {PhoneIcon} from "@heroicons/react/24/solid"
+import headerImg from "../src/assets/first.png"
+import supportImg from "../src/assets/second.jpg"
+import {FaFacebook} from 'react-icons/fa'
+import { FaBuildingColumns } from "react-icons/fa6";
+import { VscGraph } from "react-icons/vsc";
+import { BiSupport } from "react-icons/bi";
+import { FaWrench } from "react-icons/fa";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+    {/* Navigation */}
+    <nav className="w-screen h-[80px] z-10 bg-sky-200 fixed drop-shadow-md">
+      <div className="px-2 flex justify-between items-center w-full h-full">
+        <div className="flex items-center">
+          <h1 className="text-3xl font-sans text-orange-400 font-bold mr-4 sm:text-4xl">SOLARIS</h1>
+          <ul className="hidden md:flex">
+            <li><a className="ho" href="#">Home</a></li>
+            <li><a className="ho" href="#">About</a></li>
+            <li><a className="ho" href="#">How We Work</a></li>
+            <li><a className="ho" href="#">Features</a></li>
+            <li><a className="ho" href="#">Kits</a></li>
+          </ul>
+        </div>
+        <div className="hidden md:flex pr-4">
+          <button className="border-none bg-transparent text-black mr-4">Sign In</button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+
+        <div className="md:hidden mr-4">
+
+      <Bars3Icon className="w-5"/>
+        </div>
+
+      </div>
+    </nav>
+
+
+
+    {/* Header */}
+
+    <header className='w-full h-screen bg-sky-200	 flex flex-col justify-between'>
+        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+            <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
+                <p className='text-2xl'>May The Force Be With You	</p>
+                <h1 className='py-3 text-5xl md:text-7xl font-bold'>Power Generation Made Easy</h1>
+                <p className='text-2xl'>We Think Therefore We Are.</p>
+                <button className='py-3 px-6 sm:w-[60%] my-4'>Get Started</button>
+            </div>
+            <div>
+                <img className='w-full' src={headerImg} alt="/" />
+            </div>
+            <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
+            mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-sky-100
+            border border-slate-300 rounded-xl text-center shadow-xl'>
+                <p>We Are Offering</p>
+                <div className='flex justify-between flex-wrap px-4'>
+                    <p className='flex items-center px-4 py-2 text-slate-500'><FaBuildingColumns className='h-6 text-indigo-600 mr-1'  /> Reliability</p>
+                    <p className='flex items-center px-4 py-2 text-slate-500'><VscGraph className='h-6  text-indigo-600 mr-1 ' /> Consistency</p>
+                    <p className='flex items-center px-4 py-2 text-slate-500'><BiSupport className='h-6 text-indigo-600 mr-1' /> Support</p>
+                    <p className='flex items-center px-4 py-2 text-slate-500'><FaWrench className='h-6 text-indigo-600 mr-1' /> Maintenance</p>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    {/* ABOUT SECTION */}
+
+    <section className='w-full my-32'>
+        <div className='max-w-[1240px] mx-auto'>
+            <div className='text-center'>
+                <h2 className='text-5xl font-bold'>Transform Your Rooftop & Garden Into Power Plant</h2>
+                <p className='text-3xl py-6 text-gray-500'>Whoever fights monsters should see to it that in the process he does not become a monster. And if you gaze long enough into an abyss, the abyss will gaze back into you.</p>
+            </div>
+
+            <div className='grid md:grid-cols-3 gap-1 px-2 text-center'>
+                <div className='border py-8 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-orange-600'>250+</p>
+                    <p className='text-gray-400 mt-2'>Projects</p>
+                </div>
+                <div  className='border py-8 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-orange-600'>5</p>
+                    <p className='text-gray-400 mt-2'>Cities</p>
+                </div>
+                <div className='border py-8 rounded-xl shadow-xl' >
+                    <p className='text-6xl font-bold text-orange-600'>36+</p>
+                    <p className='text-gray-400 mt-2'>Megawatts per month</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    {/* SUPPORT SECTION */}
+
+    <div name='support' className='w-full mt-24'>
+      <div className='w-full h-[700px] bg-gray-800/80 absolute'>
+        <img className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" />
+      </div>
+      
+      <div className='max-w-[1240px] mx-auto text-white relative'>
+          <div className='px-4 py-12'>
+              <h2 className='text-3xl pt-8 text-slate-300 uppercase text-center'>Instalation</h2>
+              <h3 className='text-5xl font-bold py-6 text-center'>How Our Team Works With Clients</h3>
+              <p className='py-4 text-3xl text-slate-200'>By three methods we may learn wisdom: First, by reflection, which is noblest; Second, by imitation, which is easiest; and third by experience, which is the bitterest. Discipline is the bridge between goals and accomplishment.</p>
+          </div>
+
+          <div className='grid grid-cols-1 lg:grid-cols-3 relative gap-x-8 gap-y-16 px-4 pt-12 sm:pt-20 text-black'>
+              <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                      <PhoneIcon className='w-16 p-4 bg-orange-600 text-white rounded-lg mt-[-4rem]' />
+                      <h3 className='font-bold text-2xl my-6'>Provide Information</h3>
+                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                      <p className='flex items-center text-orange-600'>Step One</p>
+                  </div>
+              </div>
+              <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                      <PhoneIcon className='w-16 p-4 bg-orange-600 text-white rounded-lg mt-[-4rem]' />
+                      <h3 className='font-bold text-2xl my-6'>Custom Planning</h3>
+                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                      <p className='flex items-center text-orange-600'>Step Two </p>
+                  </div>
+              </div>
+              <div className='bg-white rounded-xl shadow-2xl'>
+                  <div className='p-8'>
+                      <PhoneIcon className='w-16 p-4 bg-orange-600 text-white rounded-lg mt-[-4rem]' />
+                      <h3 className='font-bold text-2xl my-6'>Proffesional Instalation</h3>
+                      <p className='text-gray-600 text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident iure placeat blanditiis ea sint earum hic iste quibusdam exercitationem.</p>
+                  </div>
+                  <div className='bg-slate-100 pl-8 py-4'>
+                      <p className='flex items-center text-orange-600'>Step Three </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+
+      {/* SUPPORT SECTION */}
+
+      <div name='platforms' className='w-full my-32'>
+      <div className='max-w-[1240px] mx-auto px-2'>
+        <h2 className='text-5xl font-bold text-center'>Features We Provide</h2>
+        <p className='text-2xl py-8 text-gray-500 text-center'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis,
+          ab. Officia sunt nulla aspernatur culpa, eaque tenetur excepturi
+          nostrum tempore.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 1</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 2</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 3</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 4</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 5</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 6</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 7</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+          <div className='flex'>
+            <div>
+              <CheckIcon className='w-7 mr-4 text-green-600' />
+            </div>
+            <div>
+              <h3 className='font-bold text-lg'>Feature 8</h3>
+              <p className='text-lg pt-2 pb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime deserunt voluptatibus consequatur similique
+                voluptates!
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
+
+    {/* PLANS SECTION */}
+
+    <div name='pricing' className='w-full text-white my-24'>
+      <div className='w-full h-[800px] bg-slate-900 absolute mix-blend-overlay'></div>
+
+      <div className='max-w-[1240px] mx-auto py-12'>
+
+        <div className='text-center py-8 text-slate-300'>
+          <h2 className='text-3xl uppercase'>Available Kits</h2>
+          <h3 className='text-5xl font-bold text-white py-8'>The right price for your Power Consumpiton</h3>
+          <p className='text-3xl'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia
+            laudantium odio ullam inventore aliquid ipsum quasi tenetur velit
+            voluptatum iste.
+          </p>
+        </div>
+
+        <div className='grid md:grid-cols-2'>
+
+          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+            <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Standard</span>
+            <div>
+              <p className='text-6xl font-bold py-4 flex'>120 KW<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+            </div>
+            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <div className='text-2xl'>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+
+                <button className='w-full py-4 my-4'>Get Started</button>
+            </div>
+          </div>
+          <div className='bg-white text-slate-900 m-4 p-8 rounded-xl shadow-2xl relative'>
+            <span className='uppercase px-3 py-1 bg-indigo-200 text-indigo-900 rounded-2xl text-sm'>Premium</span>
+            <div>
+              <p className='text-6xl font-bold py-4 flex'>200 KW<span className='text-xl text-slate-500 flex flex-col justify-end'>/mo</span></p>
+            </div>
+            <p className='text-2xl py-8 text-slate-500'>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+            <div className='text-2xl'>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+                <p className='flex py-4'><CheckIcon className='w-8 mr-5 text-green-600'  />Lorem, ipsum dolor.</p>
+
+                <button className='w-full py-4 my-4'>Get Started</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    {/* Footer */}
+    <div className='w-full mt-24 bg-slate-900 text-gray-300 py-y px-2'>
+        <div className='max-w-[1240px] mx-auto grid grid-cols-2 md:grid-cols-6 border-b-2 border-gray-600 py-8'>
+            <div>
+                <h6 className='font-bold uppercase pt-2'>Solutions</h6>
+                <ul>
+                    <li className='py-1'>Marketing</li>
+                    <li className='py-1'>Analytics</li>
+                    <li className='py-1'>Commerce</li>
+                    <li className='py-1'>Data</li>
+                </ul>
+            </div>
+            <div>
+                <h6 className='font-bold uppercase pt-2'>Support</h6>
+                <ul>
+                    <li className='py-1'>Pricing</li>
+                    <li className='py-1'>Documentation</li>
+                    <li className='py-1'>Guides</li>
+                </ul>
+            </div>
+            <div>
+                <h6 className='font-bold uppercase pt-2'>Company</h6>
+                <ul>
+                    <li className='py-1'>About</li>
+                    <li className='py-1'>Blog</li>
+                    <li className='py-1'>Jobs</li>
+                    <li className='py-1'>Partners</li>
+                </ul>
+            </div>
+            <div>
+                <h6 className='font-bold uppercase pt-2'>Legal</h6>
+                <ul>
+
+                    <li className='py-1'>Privacy</li>
+                    <li className='py-1'>Terms</li>
+                    <li className='py-1'>Policies</li>
+                    <li className='py-1'>Conditions</li>
+                </ul>
+            </div>
+            <div className='col-span-2 pt-8 md:pt-2'>
+                <p className='font-bold uppercase'>Subscribe to our newsletter</p>
+                <p className='py-4'>The latest news, articles, and resources, sent to your inbox weekly.</p>
+                <form className='flex flex-col sm:flex-row'>
+                    <input className='w-full p-2 mr-4 rounded-md mb-4' type="email" placeholder='Enter email..'/>
+                    <button className='p-2 mb-4'>Subscribe</button>
+                </form>
+            </div>
+        </div>
+
+        <div className='flex flex-col max-w-[1240px] px-2 py-4 mx-auto justify-between sm:flex-row text-center text-gray-500'>
+        <p className='py-4'>2024 Solaris Group. All rights reserved</p>
+        <div className='flex justify-between sm:w-[300px] pt-4 text-2xl'>
+            <FaFacebook />
+            <FaFacebook/>
+            <FaFacebook />
+            <FaFacebook />
+            <FaFacebook />
+            <FaFacebook />
+        </div>
+        </div>
+    </div>
+    </>
   );
 }
 
