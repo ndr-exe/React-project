08d74fc443c555e3213c3e7ef7ea43bbb2282186
ndr-exe <miminodar@Nodars-MacBook-Pro.local>
2024-04-01 02:53:31 +0400

    Fix active category tags styling bug

--- src/components/Marketplace/CategoryButton.jsx
+++ src/components/Marketplace/CategoryButton.jsx
@@ -1,6 +1,6 @@
 export default function CategoryButton({children,handleClick,filter,active}) {
 
-    const activeClass = active ? "bg-blue-900" : ""
+    const activeClass = active ? "bg-blue-900" : "bg-slate-800"
 
   return (
     <button 
@@ -8,7 +8,7 @@ export default function CategoryButton({children,handleClick,filter,active}) {
             !active  ?  handleClick(e,filter) : handleClick(e,filter,'remove')
 
         }}
-        className={"px-4 py-3 bg-slate-800 rounded-2xl hover:bg-blue-900 flex items-center gap-2 " + activeClass} >
+        className={"px-4 py-3 rounded-2xl hover:bg-blue-900 flex items-center gap-2 " + activeClass} >
         {children}
     </button>
   )
